91409381fab90e56f0915c3de282ac8059d0381e
Laimis <laimiss91@gmail.com>
2019-05-03 20:09:56 +0300

    Add project files.

---
+++ DataOrganizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataOrganizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
